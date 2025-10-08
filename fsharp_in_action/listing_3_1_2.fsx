a68c3a3252f54f2289151de6ea15eda7f849ccf3
Sunil Udupi <udpsunil@gmail.com>
2025-10-08 21:32:49 +0530

    Add greeting functionality with full name construction

--- fsharp_in_action/listing_3_1_2.fsx
+++ fsharp_in_action/listing_3_1_2.fsx
@@ -4,3 +4,17 @@ let doACalculation theNumber =
     answer
 
 printfn $"{doACalculation 20}"
+
+let greetingText = 
+    let fullName = 
+        let fname = "Sunil"
+        let lname = "Udupi"
+        $"{fname} {lname}"
+    $"Greetings, {fullName}"
+
+
+let greetingTextWithFunction person = 
+    let makeFullName fname lname = 
+        $"{fname} {lname}"
+    let fullName = makeFullName "Sunil" "Udupi"
+    $"Greetings, {fullName} from {person}."
