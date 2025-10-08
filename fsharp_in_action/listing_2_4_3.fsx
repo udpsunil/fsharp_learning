0a9a247e9b5ff47a21a90a13dbacd52d61ec410d
Sunil Udupi <udpsunil@gmail.com>
2025-10-08 20:22:42 +0530

    Add greeting functions and examples in listing 2.5.5 and listing 2.4.3

---
+++ fsharp_in_action/listing_2_4_3.fsx
@@ -0,0 +1,4 @@
+let name = "Sunil Udupi"
+let date = System.DateTime.Now
+
+printfn $"Hello, from F#! My name is {name}, the time is {date}"
---
+++ fsharp_in_action/listing_2_5_5.fsx
@@ -0,0 +1,4 @@
+let greetPerson name age = $"Hello, {name}. You are {age} years old."
+
+let greetings = greetPerson "Sunil" 42
+printfn $"{greetings}"
